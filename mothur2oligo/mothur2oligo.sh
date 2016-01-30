cbf5c02c71e367d8cf46da5a19e558a3130e1093
Michelle Berry <michberr8@gmail.com>
2016-01-30 15:04:13 -0500

    Update mothur2oligo.sh
    learning to write shell scripts for real

--- mothur2oligo/mothur2oligo.sh
+++ mothur2oligo/mothur2oligo.sh
@@ -1,17 +1,20 @@
+#!/bin/sh
+# USAGE: ./mothur2oligo.sh
 # This is a shell script for transforming mothur output to appropriate format for 
 # A. murat Eren's oligotyping pipeline 
 
 # Set variables
-groups = #'stability.contigs.good.groups'
-redundant = #'stability.trim.contigs.good.unique.good.filter.pick.redundant.fasta'
+# Adjust these file names to your own study - these are the files from the mothur SOP
+groups="stability.contigs.good.groups"
+redundant="stability.trim.contigs.good.unique.good.filter.pick.redundant.fasta"
 
 # Call mothur script for generating deuniqued fasta file for a specific lineage
 mothur getlineage.mothur
 
 # Make a file which maps sample names to sequence headers
-cat groups | sed 's/_/:/g' > intermediate1
-paste groups intermediate1 | awk 'BEGIN{FS="\t"}{print $1"\t"$2"_"$3}' > intermediate2
+cat $groups | sed 's/_/:/g' > intermediate1
+paste $groups intermediate1 | awk 'BEGIN{FS="\t"}{print $1"\t"$2"_"$3}' > intermediate2
 
 # Perl script to rename the headers of the fasta to include the sample name at the beginning
-perl renamer.pl redundant intermediate2
+perl renamer.pl $redundant intermediate2
 
