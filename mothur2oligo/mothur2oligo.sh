fe9e207445ba025b8d375f399a22714ee9d2293a
Michelle Berry <michberr8@gmail.com>
2016-02-02 23:15:12 -0500

    Added more comments to mothur2oligo.sh

--- mothur2oligo/mothur2oligo.sh
+++ mothur2oligo/mothur2oligo.sh
@@ -11,10 +11,11 @@ redundant="stability.trim.contigs.good.unique.good.filter.pick.redundant.fasta"
 # Call mothur script for generating deuniqued fasta file for a specific lineage
 mothur getlineage.mothur
 
-# Make a file which maps sample names to sequence headers
+# Replace all "_" in fasta header with a ":"
 cat $groups | sed 's/_/:/g' > intermediate1
+# Make a file which maps sample names to sequence headers
 paste $groups intermediate1 | awk 'BEGIN{FS="\t"}{print $1"\t"$2"_"$3}' > intermediate2
 
-# Perl script to rename the headers of the fasta to include the sample name at the beginning
+# Perl script to rename the headers of the fasta to include the sample name at the beginning followed by a "_"
 perl renamer.pl $redundant intermediate2
 
