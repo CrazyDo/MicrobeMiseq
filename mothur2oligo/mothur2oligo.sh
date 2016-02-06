4debf05e9666b9041ae0d37c093c27798be1f8d2
Michelle Berry <michberr8@gmail.com>
2016-02-06 13:36:14 -0500

    Removed mothur batchfile Changed mothur2oligo.sh to call mothur in command line mode

--- mothur2oligo/mothur2oligo.sh
+++ mothur2oligo/mothur2oligo.sh
@@ -1,15 +1,29 @@
-#!/bin/sh
-# USAGE: ./mothur2oligo.sh
+# USAGE: sh mothur2oligo.sh
 # This is a shell script for transforming mothur output to appropriate format for 
 # A. murat Eren's oligotyping pipeline 
 
-# Set variables
-# Adjust the groups file name to your own study - this is the filet from the mothur SOP
-groups="stability.contigs.good.groups"
-redundant="*.pick.redundant.fasta"
+## Set variables
 
-# Call mothur script for generating deuniqued fasta file for a specific lineage
-mothur getlineage.mothur
+# Adjust the file names to your own study - these are the files from the mothur SOP
+taxonomy="stability.trim.contigs.good.unique.good.filter.unique.precluster.pick.pds.wang.taxonomy"
+groups="stability.contigs.good.groups"
+fasta="stability.trim.contigs.good.unique.good.filter.fasta"
+name="stability.trim.contigs.good.names"
+processors=2
+
+# Set the taxon you want to select, separate taxonomic levels with ";" 
+# Do not touch inner and outer quotes
+taxon="'Bacteria;Firmicutes;'"
+
+
+################################
+########## Script  #############
+################################
+
+redundant=$(echo ${fasta} | sed 's/.fasta//') 
+
+# Call mothur commands for generating deuniqued fasta file for a specific lineage
+mothur "#set.current(processors=$processors); get.lineage(taxonomy=$taxonomy, taxon=$taxon, name=$name); list.seqs(taxonomy=current); get.seqs(accnos=current, fasta=$fasta); deunique.seqs(fasta=current, name=current)"
 
 # Replace all "_" in fasta header with a ":"
 cat $groups | sed 's/_/:/g' > intermediate1
@@ -17,5 +31,5 @@ cat $groups | sed 's/_/:/g' > intermediate1
 paste $groups intermediate1 | awk 'BEGIN{FS="\t"}{print $1"\t"$2"_"$3}' > intermediate2
 
 # Perl script to rename the headers of the fasta to include the sample name at the beginning followed by a "_"
-perl renamer.pl $redundant intermediate2
+perl renamer.pl $redundant.pick.redundant.fasta intermediate2 
 
