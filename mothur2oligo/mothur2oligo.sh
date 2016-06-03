6c52df9b06dec172f9b8dfdf3aa93d06a53f2b96
Michelle Berry <michberr8@gmail.com>
2016-06-03 15:32:28 -0400

    Script now utilizes count file instead of names/groups

--- mothur2oligo/mothur2oligo.sh
+++ mothur2oligo/mothur2oligo.sh
@@ -6,9 +6,8 @@
 
 # Adjust the file names to your own study - these are the files from the mothur SOP
 taxonomy="stability.trim.contigs.good.unique.good.filter.unique.precluster.pick.pds.wang.taxonomy"
-groups="stability.contigs.good.groups"
-fasta="stability.trim.contigs.good.unique.good.filter.fasta"
-name="stability.trim.contigs.good.names"
+fasta="fasta=stability.trim.contigs.good.unique.good.filter.unique.precluster.pick.fasta"
+count="stability.trim.contigs.good.unique.good.filter.unique.precluster.denovo.uchime.pick.count_table"
 processors=2
 
 # Set the taxon you want to select, separate taxonomic levels with ";" 
@@ -20,10 +19,11 @@ taxon="'Bacteria;Firmicutes;'"
 ########## Script  #############
 ################################
 
-redundant=$(echo ${fasta} | sed 's/.fasta//') 
+redundantFasta=$(echo ${fasta}.pick.redundant.fasta | sed 's/.fasta//')
+groups=$(echo ${count}.pick.redundant.groups | sed 's/.count_table//') 
 
 # Call mothur commands for generating deuniqued fasta file for a specific lineage
-mothur "#set.current(processors=$processors); get.lineage(taxonomy=$taxonomy, taxon=$taxon, name=$name); list.seqs(taxonomy=current); get.seqs(accnos=current, fasta=$fasta); deunique.seqs(fasta=current, name=current)"
+mothur "#set.current(processors=$processors); get.lineage(taxonomy=$taxonomy, taxon=$taxon, count=$count); list.seqs(count=current); get.seqs(accnos=current, fasta=$fasta); deunique.seqs(fasta=current, count=current)"
 
 # Replace all "_" in fasta header with a ":"
 cat $groups | sed 's/_/:/g' > intermediate1
@@ -31,5 +31,5 @@ cat $groups | sed 's/_/:/g' > intermediate1
 paste $groups intermediate1 | awk 'BEGIN{FS="\t"}{print $1"\t"$2"_"$3}' > intermediate2
 
 # Perl script to rename the headers of the fasta to include the sample name at the beginning followed by a "_"
-perl renamer.pl $redundant.pick.redundant.fasta intermediate2 
+perl renamer.pl $redundantFasta intermediate2 
 
